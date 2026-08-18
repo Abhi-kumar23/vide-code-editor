ea90944f11dda9d8c099ed414f5dba0859defd96
Abhi-kumar23 <abhi.akp231@gmail.com>
2026-08-19 00:13:44 +0530

    fix: theme change toggle and correct provider hierarchy in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,12 +29,12 @@ export default async function RootLayout({
   const session = await auth()
   return (
 
-    <SessionProvider session={session}>
     <html
       lang="en" suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+      <SessionProvider session={session}>
          <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -43,8 +43,8 @@ export default async function RootLayout({
           >
         {children}
         </ThemeProvider>
+        </SessionProvider>
         </body>
     </html>
-    </SessionProvider>
   );
 }
